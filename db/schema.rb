b0be5da2c7f7b745ac30586bbcceea8d82b274cc
Adam Angel <adamangel@Adams-MBP.home>
2023-02-25 20:36:04 -0500

    added description, validation for title

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_02_26_012903) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
